a4a3f65421e7a19b40b5bc30be9fcf77d54087e2
anakarenrodriguezsauceda <anakarenrodriguezsauceda@gmail.com>
2022-12-12 23:17:51 -0600

    Se creó proyecto con vue y creación de botones dinámicos por un ciclo FOR

---
+++ src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>Horarios: Lunes a viernes de 10am a 6pm
+      Sabados y domingos 10am a 2pm
+    </h1>
+    <h1>Costo de la consulta: $100</h1>
+    <h1>Miercoles de consulta a mitad de precio</h1>
+    <h1>Contactanos: Tel: 4643987498
+      correo: arftd@gmail.com
+    </h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Mis Citas - Consultorio Familiar</h1>
+  </div>
+  
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
